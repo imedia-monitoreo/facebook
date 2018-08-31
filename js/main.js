e10258b3a5f3dfac49d8197e42a777df162ffc1d
Betsy Vidal <ing.betsyvies@gmail.com>
2018-08-31 16:45:19 -0500

    Login with facebook

---
+++ js/main.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', function() {
+  var loginBtn = document.getElementById('log-in');
+
+  loginBtn.addEventListener('click', facebookLogin);
+
+  function facebookLogin() {
+    provider = new firebase.auth.FacebookAuthProvider();
+
+    provider.setCustomParameters({
+        'display': 'popup'
+    });
+
+    firebase.auth().signInWithPopup(provider)
+      .then((result) => {
+          // The signed-in user info.
+          var user = result.user;
+          // Mostramos su contenido
+          console.log(user);
+          // Llamamos a la funcion
+          saveData(result.user);
+          window.location.assign('index.html');
+      })
+  }
+
+  function saveData(user) {
+    var users = {
+      uid: user.uid,
+      name: user.displayName,
+      email: user.email,
+      photo: user.photoURL
+    };
+    firebase.database().ref('bd/' + user.uid).set(users);
+  }
+})
